c6f4abd7548aae5edc52f61689a4cd370a4d6e35
Xiuyuan Liu <liuxiuyuan666@gmail.com>
2018-03-13 19:23:05 +0800

    bla

---
+++ Assets/Boid.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+
+    public float mass = 1.0f;
+    public float maximumSpeed = 10.0f;
+    public Vector3 velocity;
+    public GameObject Leader;
+    private Vector3 offset;
+
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - Leader.transform.position;
+        offset = Quaternion.Inverse(Leader.transform.rotation) * offset;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {    
+        velocity = Seek(Leader.transform.TransformPoint(0, 0, 1000));
+        Leader.transform.Translate(velocity*Time.deltaTime);
+
+	}
+
+    public Vector3 Seek(Vector3 Target)
+    {
+        //Vector3 toTarget = target.transform.position - transform.position;
+        Vector3 desired = Target - transform.position;
+        desired.Normalize();
+        desired *= maximumSpeed;
+        return desired - velocity;
+    }
+
+    public void OffsetPursue(Vector3 leader)
+    {
+        
+    }
+}
---
+++ Assets/EagleSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EagleSpawner : MonoBehaviour {
+
+    public float gap = 20;
+    public float followers = 2;
+    public GameObject Follower;
+    public GameObject Leader;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Awake()
+    {
+        for (int i = 0; i <= followers; i++)
+        {
+            GameObject eagle = GameObject.Instantiate(Follower);
+            eagle.transform.rotation = Leader.transform.rotation;
+            eagle.transform.position = Leader.transform.TransformPoint(new Vector3(gap*i, 0, -gap*i));
+            eagle.transform.position = Leader.transform.TransformPoint(new Vector3(-gap*i, 0, -gap*i));
+        }
+        
+
+    }
+}
